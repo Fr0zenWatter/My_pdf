9cf68e412fffcf9136b82f72083627cb4a796bd1
571244460 <976609856@qq.com>
2022-11-15 16:20:15 +0800

    6

---
+++ process.py
@@ -0,0 +1,23 @@
+from wxauto import *
+import get_pdf_file
+
+def run_process(wx,who):
+    wx.ChatWith(who)
+    #转到文件传输窗口
+    wx.LoadMoreMessage
+    msgs = wx.GetAllMessage
+    #获取消息
+    msg = msgs[-1][1]
+    #获取最新消息的，消息部分
+    #分割
+    msg_split = msg.split()
+    get_pdf_file.get_pdf_name(str(msg_split[0]),str(msg_split[1]))
+
+
+wx = WeChat()
+who = "文件传输助手"
+run_process(wx,who)
+
+
+
+
